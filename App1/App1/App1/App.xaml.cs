79c057bfb32f05d17b6fd7cdfe2d0f20e78ad3b7
Lawrence Pina <lawrence@colada.com>
2018-10-31 19:30:09 -0700

    App center

--- App1/App1/App1/App.xaml.cs
+++ App1/App1/App1/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using App1.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -20,6 +23,7 @@ namespace App1
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=4c0a2400-3aab-41bb-8d85-e40a719e6023;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
